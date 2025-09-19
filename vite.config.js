829e2bf3eb2ec8972479204543b75590c35d8408
Husam Droubi <husam.droubi@gmail.com>
2025-09-19 13:45:01 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/e-house-configurator/',  // 👈 important for GitHub Pages
-  server: { port: 5173, open: true }
-});
+  base: '/e-house-configurator/',  // 👈 required for GitHub Pages
+})
